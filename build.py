c3e906529a45b357083e25ef535456b227f239be
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-01-23 15:57:46 -0600

    Fix build.py

--- build.py
+++ build.py
@@ -1,7 +1,7 @@
 from cpt.packager import ConanMultiPackager
 
-
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name="squashfuse:shared")
+    install_deps_script = "sudo apt-get -qq update && sudo apt-get -qq install -y libfuse-dev"
+    builder = ConanMultiPackager(docker_entry_script=install_deps_script)
+    builder.add_common_builds()
     builder.run()
