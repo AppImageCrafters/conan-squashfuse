b2a0ec642506cfccbd16f0667fcb178ebd0868d0
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-04-27 12:07:39 -0500

    Change upload url

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    remotes=[("https://api.bintray.com/conan/bincrafters/public-conan", "yes", "bincrafters")]
+    builder = ConanMultiPackager(remotes=remotes, build_policy="missing")
     builder.add_common_builds()
     builder.run()
