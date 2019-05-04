87a6fa8c0d111b24f786c0b45799121d2b9f37e8
Alexis Lopez Zubieta <contact@azubieta.net>
2019-05-03 22:42:16 -0500

    Set upload policy to outdated

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     remotes=[("https://api.bintray.com/conan/bincrafters/public-conan", "yes", "bincrafters")]
-    builder = ConanMultiPackager(remotes=remotes, build_policy="missing")
+    builder = ConanMultiPackager(remotes=remotes, build_policy="outdated")
     builder.add_common_builds()
     builder.run()
