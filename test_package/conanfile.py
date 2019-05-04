ccae75dda3a923b4001b58852521fc6ea7f130ff
Alexis Lopez Zubieta <contact@azubieta.net>
2019-05-03 23:09:09 -0500

    Remove unrequired cmake generator

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -9,12 +9,9 @@ class SquashfuseTestConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is
-        # in "test_package"
         cmake.configure()
         cmake.build()
 
     def test(self):
         if not tools.cross_building(self.settings):
-            os.chdir("bin")
             self.run(".%sexample" % os.sep)
